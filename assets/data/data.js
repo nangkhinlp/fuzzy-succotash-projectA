4999b9a070e497b64de065baee87e6f88af7d943
nangkhinlp <nangkhinlp@gmail.com>
2026-02-18 17:08:37 -0600

    Add files via upload

---
+++ assets/data/data.js
@@ -0,0 +1,30 @@
+//Current Top 10 Songs
+
+const jsObjectData = [
+    { id: 1, artist: "Bad Bunny", title: "DTMF", album: "DeBÍ TiRAR MáS FOToS"},
+    { id: 2, artist: "Bad Bunny", title: "Balle Inolvidable", album: "DeBÍ TiRAR MáS FOToS"},
+    { id: 3, artist: "Olivia Dean", title: "Man I Need", album: "The Art of Loving"},
+    { id: 4, artist: "Ella Langley", title: "ChoosIn' Texas", album: "Dandelion"},
+    { id: 5, artist: "Bad Bunny", title: "Nuevayol", album: "DeBÍ TiRAR MáS FOToS"},
+    { id: 6, artist: "Alex Warren", title: "Ordinary", album: "You'll Be Alright, Kid"},
+    { id: 7, artist: "Bad Bunny", title: "Titi Me Pregunto", album: "DeBÍ TiRAR MáS FOToS"},
+    { id: 8, artist: "Taylor Swift", title: "Opalite", album: "The Life of a Showgirl"},
+    { id: 9, artist: "Huntrix", title: "Golden", album: "Kpop Demon Hunter (Soundtrack)"},
+    { id: 10, artist: "Bruno Mars", title: "I Just Might", album: "The Romantic"}
+  ];
+
+
+//Top 10 Artists of the 21st Century
+
+const jsArrayData = [
+  "Taylor Swift",
+  "Drake",
+  "Rihanna",
+  "Post Malone",
+  "Eminem",
+  "The Weeknd",
+  "Beyonce",
+  "Justin Bieber",
+  "Bruno Mars",
+  "Usher"
+];
